bac8459dbe057157452b9c25dc64e66a38774022
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-07 14:47:43 +0800

    added main layout draft

--- hannahsport22/src/layout/public-layout.jsx
+++ hannahsport22/src/layout/public-layout.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MainLayout(props){
     return(
-        ({/*nav */})
-
+        ({/*nav */}),
+        ({props}),
         ({/*footer */})
     )
 }
