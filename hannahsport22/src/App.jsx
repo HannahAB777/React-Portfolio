7037b58d9561015f8605dabaf0ec926e11723c92
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-07 14:42:45 +0800

    basic layout

---
+++ hannahsport22/src/App.jsx
@@ -0,0 +1,32 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/about";
+import Home from "./components/home";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
+import MainLayout from "./layout/public-layout";
+
+function App() {
+  //about
+  //projects
+  //homepage
+  //contact me
+  return (
+    <BrowserRouter>
+
+    <MainLayout>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+
+</MainLayout>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
