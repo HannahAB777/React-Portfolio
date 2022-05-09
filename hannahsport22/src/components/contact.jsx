80e45c4983acdb718b8f72325b87022cc5d7b73f
HannahAB777 <hannah.allisonbatt@gmail.com>
2022-05-09 13:49:26 +0800

    started on contact

--- hannahsport22/src/components/contact.jsx
+++ hannahsport22/src/components/contact.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function contact(){
     return(
-        <div>Contact me</div>
+        <div>
+        <Container>
+        <Row>
+        <Col>
+        </Col>
+        </Row>
+        <Row>
+        <Col></Col>
+        </Row>
+        <Row>
+        <Col></Col>
+        </Row></Container>
+        </div>
     )
 }
